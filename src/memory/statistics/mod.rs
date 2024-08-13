e32a6020d0010aeefa973afa3470cd90f3cef6db
Ian <ian.diks@uni-saarland.de>
2024-08-13 06:54:20 +0000

    use borrow in stats generation

--- src/memory/statistics/mod.rs
+++ src/memory/statistics/mod.rs
@@ -4,14 +4,14 @@ use anndata_memory::IMAnnData;
 
 use crate::shared::{statistics::number::whole, Direction};
 
-pub fn compute_number(adata: IMAnnData, direction: Direction) -> anyhow::Result<Vec<u32>> {
+pub fn compute_number(adata: &IMAnnData, direction: Direction) -> anyhow::Result<Vec<u32>> {
     let x = adata.x();
     let data_inner = x.0.read_inner();
     let data = data_inner.deref();
     whole(data, direction)
 }
 
-pub fn compute_sum(adata: IMAnnData, direction: Direction) -> anyhow::Result<Vec<f64>> {
+pub fn compute_sum(adata: &IMAnnData, direction: Direction) -> anyhow::Result<Vec<f64>> {
     let x = adata.x();
     let data_inner = x.0.read_inner();
     let data = data_inner.deref();
